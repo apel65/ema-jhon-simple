ab90acca67074745e2fcb807d92d2e4e74e9e85f
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-07-02 11:13:42 +0800

    finally finished website ema-jhon-simple

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,30 +4,29 @@ import './Header.css';
 import { useAuth } from '../Login/useAuth';
 import { Link } from 'react-router-dom';
 
-
 const Header = () => {
-    const auth = useAuth();
-    console.log(auth)
-    return (
-        <div className="header">
-            <img src={logo} alt=""/>
-        
-            <nav>
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/inventory">Orders</a>
+	const auth = useAuth();
+	console.log(auth);
+	return (
+		<div className="header">
+			<img src={logo} alt="" />
 
-                {
-                    auth.user && <span style={{color:'yellow'}}>Welcome {auth.user.name}</span>
-                }
-                {
-                    auth.user ?  <a href="/login">Sign Out</a> :
-                   <a href="/login">Sign In</a>
-                  
-                }
-            </nav>
-        </div>
-    );
+			<nav>
+				<a href="/shop">Shop</a>
+				<a href="/review">Order Review</a>
+				<a href="/orders">Order History</a>
+
+				{auth.user && (
+					<span style={{ color: 'yellow' }}>Welcome {auth.user.name}</span>
+				)}
+				{auth.user ? (
+					<a href="/login">Sign Out</a>
+				) : (
+					<a href="/login">Sign In</a>
+				)}
+			</nav>
+		</div>
+	);
 };
 
 export default Header;
--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Inventory = () => {
-    return (
-        <div>
-            <h2>This is inventory</h2>
-        </div>
-    );
+	return (
+		<div>
+			<h2>Order History comming soon....</h2>
+		</div>
+	);
 };
 
 export default Inventory;
